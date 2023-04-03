b9ecaf2cd9628ab13c534ddbffb1b6ebe9f22535
Alice Wood <alicesophiewood@gmail.com>
2023-04-03 15:25:42 +0100

    Add file 2 and Exercise 1

---
+++ directory_2.rb
@@ -0,0 +1,35 @@
+
+def input_students
+  puts "Please enter the name of students"
+  puts "To finish, just hit return twice"
+
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+
+    name = gets.chomp
+  end
+  students
+end
+
+def print_header
+  puts "the students of Villains Academy"
+  puts "---------------"
+end
+
+def print(names)
+  names.each.with_index(1) do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+ puts "Overall, we have #{names.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
