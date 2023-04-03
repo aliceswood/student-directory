0ec0d3fa4910fd5c8dff15acebebe78d32f50573
Alice Wood <alicesophiewood@gmail.com>
2023-04-03 16:03:30 +0100

    Exercise 4

--- directory_2.rb
+++ directory_2.rb
@@ -1,17 +1,17 @@
+@students = []
 
 def input_students
   puts "Please enter the name of students"
   puts "To finish, just hit return twice"
 
-  students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
 
     name = gets.chomp
   end
-  students
+  @students
 end
 
 def print_header
@@ -19,16 +19,16 @@ def print_header
   puts "---------------"
 end
 
-def print(names)
-  names.each.with_index(1) do |student, index|
-    if student[:name].length < 7
-      puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end
+def print(name)
+  n = 0
+  while n < name.count
+  puts "#{@students[n][:name]} (#{@students[n][:cohort]} cohort)"
+  n += 1
   end
 end
 
-def print_footer(names)
- puts "Overall, we have #{names.count} great students"
+def print_footer(name)
+ puts "Overall, we have #{name.count} great students"
 end
 
 students = input_students
