f738f1df06a7e3e766a19d86c85d9dac2a1ce7fe
Alice Wood <alicesophiewood@gmail.com>
2023-04-03 11:15:37 +0100

    Refactor make array and add iteration

--- directory.rb
+++ directory.rb
@@ -1,17 +1,23 @@
-student_count = 11
+
+students = [
+  "Dr Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddie Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
 
 puts "the students of Villains Academy"
 puts "---------------"
-puts "Dr Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddie Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
 
-print "Overall, we have #{student_count} great students"
+students.each do |student|
+  puts student
+end
+
+print "Overall, we have #{student.count} great students"
